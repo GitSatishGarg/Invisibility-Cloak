e4ced572160d2cfdb3b59012649bc7e0c03827ca
GitSatishGarg <satish.brainiac@gmail.com>
2021-06-28 16:50:59 +0530

    Upload

---
+++ app.py
@@ -0,0 +1,50 @@
+# Import libraries
+import numpy as np
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+time.sleep(2)
+background = 0
+
+# Capturing Background
+for i in range(30):
+    ret, background = cap.read()
+
+while(cap.isOpened()):
+    ret, img = cap.read()
+
+    if not ret:
+        break;
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # HSB
+
+    #HSV values
+    lower_red = np.array([0, 120, 70])
+    upper_red = np.array([10, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([170, 120, 70])
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask1 = mask1 + mask2 # OR 1 or x
+
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN,
+                                np.ones((3, 3), np.uint8), iterations = 5) # Removing Noise
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE,
+                                np.ones((3, 3), np.uint8), iterations = 1)
+
+    mask2 = cv2.bitwise_not(mask1)  # Evryting except cloak
+
+    res1 = cv2.bitwise_and(background, background, mask=mask1) # Used for segemntation of color
+    res2 = cv2.bitwise_and(img, img, mask=mask2) # Used to substitute the cloak part
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+    
+    cv2.imshow('Ron Im Invisible!', final_output)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
